cf40aa3e2b898cc8d8b87dac146b0a5a69cf80a7
ss749236 <ss749236@gmail.com>
2023-08-13 17:35:06 +0530

    selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css';
 import MovieCard from './component/movie-card';
 
 
+
+
+
 const APIUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const SearchMovieApi = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 function App() {
@@ -50,7 +53,9 @@ function App() {
 
 
   return (
+
     <div className='App'>
+
       <div className='search-box'>
         <div className='title'>Movies </div>
         <input value={search} onChange={handleInput} type='search' placeholder='search movie here' />
--- src/component/movie-card.js
+++ src/component/movie-card.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./movie-card.css"
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+
+
+
+
+
+
 const MovieCard = (props) => {
+    const [selectedMovie, setSelectedMovie] = useState([]);
+    const { id } = useParams();
+
+
+
+
+    const handleClick = () => {
+        axios.get(APIUrl).then((response) => {
+            setSelectedMovie(response.data.results);
+
+        })
+    }
+
 
     const IMGPATH = "https://image.tmdb.org/t/p/w1280/";
+    const APIUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1/${id}`;
+
+
     return (
-        <div className="card">
-            <div className="poster">
-                <img src={IMGPATH + props.poster_path} alt="" />
-            </div>
-            <div className="info">
-                <p className="title">{props.title}</p>
-                <p className="vote">{props.vote_average}</p>
+        <button onClick={handleClick}>
+            {!selectedMovie}
+            <div className="card">
+                <div className="poster">
+                    <img src={IMGPATH + props.poster_path} alt="" />
+                </div>
+                <div className="info">
+                    <p className="title">{props.title}</p>
+                    <p className="vote">{props.vote_average}</p>
+                    {/* <p>{props.overview}</p> */}
+
+                </div>
             </div>
 
-        </div>
+
+
+        </button>
+
+
     )
 }
 export default MovieCard;
