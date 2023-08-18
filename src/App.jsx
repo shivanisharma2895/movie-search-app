10482053614a2ebcf8a94b30e4bad9a4c8b1964b
ss749236 <ss749236@gmail.com>
2023-08-18 13:34:03 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,15 @@
-import { useEffect, useState } from 'react';
-import axios from "axios"
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import MovieCard from './component/movie-card';
-
-
-
-
-
-
-const APIUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const SearchMovieApi = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+import MovieApp from './component/main-panel';
+import SelectedMovie from './component/selected-movie';
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState("");
-
-  const [id, setId] = useState('');
-
-
-  const getMovies = () => {
-    axios.get(APIUrl).then((response) => {
-      console.log(response.data.results);
-      setMovies(response.data.results);
-      // setId(response.data.results.id);
-    }
-    ).catch((error) => {
-      console.log("error", error);
-    })
-  }
-
-
-  useEffect(() => {
-    setMovies([]);
-    if (search === "") {
-      getMovies();
-    } else {
-      getSearchedMovie();
-    }
-
-  }, [search]);
-
-  const getSearchedMovie = () => {
-
-    console.log(SearchMovieApi + search);
-    axios.get(SearchMovieApi + search).then((res) => {
-      console.log(res.data.results);
-      setMovies(res.data.results);
-    }).catch((error) => {
-      console.log("error", error);
-    })
-  }
-
-  const handleInput = (e) => {
-    console.log("value", e.target.value)
-    setSearch(e.target.value);
-  }
-
-
-  const SelectMovie = () => {
-    axios.get(APIUrl).then((res) => {
-      console.log(res.data.results.id);
-      setId(res.data.results.id);
-    }
-    )
-  }
-
-
   return (
-
-    <div className='App'>
-
-      <div className='search-box'>
-        <div className='title'>Movies </div>
-        <input value={search} onChange={handleInput} type='search' placeholder='search movie here' />
-      </div>
-
-
-      <div className='movies' onClick={() => SelectMovie(movies.id)} >
-
-        {movies.length && movies.map((movie) =>
-          <MovieCard key={movie.id} {...movie} />
-        )}
-      </div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MovieApp />} />
+        <Route path='/select' element={<SelectedMovie />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/main-panel.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+import { useNavigate } from 'react-router-dom';
+import MovieCard from './movie-card';
+import SelectedMovie from './selected-movie';
+
+const APIUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SearchMovieApi = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+function MovieApp() {
+    const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const [id, setId] = useState('');
+    const navigate = useNavigate();
+
+
+    const getMovies = () => {
+        axios.get(APIUrl).then((response) => {
+            console.log(response.data.results);
+            setMovies(response.data.results);
+            // setId(response.data.results.id);
+        }
+        ).catch((error) => {
+            console.log("error", error);
+        })
+    }
+
+
+    useEffect(() => {
+        setMovies([]);
+        if (search === "") {
+            getMovies();
+        } else {
+            getSearchedMovie();
+        }
+
+    }, [search]);
+
+    const getSearchedMovie = () => {
+
+        console.log(SearchMovieApi + search);
+        axios.get(SearchMovieApi + search).then((res) => {
+            console.log(res.data.results);
+            setMovies(res.data.results);
+        }).catch((error) => {
+            console.log("error", error);
+        })
+    }
+
+    const handleInput = (e) => {
+        console.log("value", e.target.value)
+        setSearch(e.target.value);
+    }
+
+
+    const SelectMovie = () => {
+        axios.get(APIUrl).then((res) => {
+            console.log(res.data.results.id);
+            setId(res.data.results.id);
+            const status = res.data.results.status
+            console.log(status);
+
+            if (movies.id === id) {
+                navigate("/select");
+            }
+
+        }
+        ).catch((error) => {
+            console.log("error", error);
+        })
+
+    }
+
+
+    return (
+
+        <div className='App'>
+
+            <div className='search-box'>
+                <div className='title'>Movies </div>
+                <input value={search} onChange={handleInput} type='search' placeholder='search movie here' />
+            </div>
+
+
+            <div className='movies' onClick={() => SelectMovie(movies.id)} >
+
+                {movies.length && movies.map((movie) =>
+                    <>  <MovieCard key={movie.id} {...movie} />
+                    </>
+
+
+
+                )}
+                <>
+                    {movies.map((movie) => <SelectedMovie {...movie} />)}
+                </>
+            </div>
+
+        </div>
+    );
+}
+
+export default MovieApp;
